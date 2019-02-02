48ffbf55310bd512054c18759779f714d4b29ff9
João Neves <joaoneves@fakeemail.com>
2019-02-02 23:43:29 +0000

    Added feet data

---
+++ tensorflow/custom_object_detection/README.md
@@ -0,0 +1,2 @@
+#https://tensorflow-object-detection-api-tutorial.readthedocs.io/en/latest/training.html
+
---
+++ tensorflow/custom_object_detection/addons/gather_images.py
@@ -0,0 +1,7 @@
+#googleimagesdownload --keywords "suit lowerbody" --limit 200
+
+# For more than 100 images
+#Installed the chromium-chromedriver: sudo apt-get install chromium-chromedriver
+#Adding the path to the selenium line.
+#Line 167 in ./google_images_download/google_images_download.py to:
+#browser = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
